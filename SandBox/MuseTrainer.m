33a56afecc64cbd402784002313a1ce2b577efc2
Ava Lakmazaheri <alakmazaheri@olin.edu>
2017-06-07 17:54:27 -0400

    testing eval on live streams

--- SandBox/MuseTrainer.m
+++ SandBox/MuseTrainer.m
@@ -1,19 +1,5 @@
 % Load EEG and Marker data from XDF
-EEG = pop_loadxdf('C:\Users\alakmazaheri\Desktop\markertest1.xdf', 'streamtype', 'signal');
-x = EEG;
-
-% Sync marker latencies
-eventlen = length(x.event);
-latencies = zeros(eventlen, 1);
-for i = 1:eventlen
-    curr = x.event(i).latency + x.xmin*x.srate; % Add data offset in samples
-    EEG.event(i).latency = curr;
-end
-
-% Remove accelerometer channels
-EEG.nbchan = 4;
-EEG.data = EEG.data(1:4,:);
-EEG.chanlocs(5:6) = [];
+EEG = preprocess('C:\Users\alakmazaheri\Desktop\markertest1.xdf');
 
 % Define custom approach (parameters based on OpenVibe Graz Motor Imagery Simulator)
 myapproach = {'CSP' 'SignalProcessing',{'FeatureExtraction',{'PatternPairs',2}, 'EpochExtraction',[0.1 1.5],'FIRFilter',[7 8 12 13]}};
@@ -21,3 +7,10 @@ myapproach = {'CSP' 'SignalProcessing',{'FeatureExtraction',{'PatternPairs',2}, 
 % Learn predictive model
 [trainloss,lastmodel,laststats] = bci_train('Data',EEG,'Approach',myapproach,'TargetMarkers',{'770','769'}, 'EvaluationMetric', 'mse'); 
 disp(['training mis-classification rate: ' num2str(trainloss*100,3) '%']);
+
+% Test on itself
+[prediction,loss,teststats,targets] = bci_predict(lastmodel,EEG);
+
+disp(['test mis-classification rate: ' num2str(loss*100,3) '%']);
+disp(['  predicted classes: ',num2str(round(prediction{2}*prediction{3})')]);  % class probabilities * class values
+disp(['  true classes     : ',num2str(round(targets)')]);
--- SandBox/inspect_data.m
+++ SandBox/inspect_data.m
@@ -1,17 +1,25 @@
-figure
-zoom xon
-%ylim([0 6])
-plot(EEG.data','DisplayName','EEG.data')
+figure; zoom xon
+% View all channels
+for i = 1:4
+    subplot(4,1,i)
+     plot(EEG.data(i,:)', 'k','DisplayName','EEG.data')
+end
+
+% Select cleanest channels to display with markers
+figure; zoom xon
+plot(EEG.data([1 4],:)','DisplayName','EEG.data')
+
+count = 0;
 
 eventlen = length(EEG.event);
-
 for i = 1:eventlen
     t = EEG.event(i).latency;
     mark = sprintf('%d', str2double(EEG.event(i).type));
-    disp(mark)
 
     if(strcmp(mark,'770') || strcmp(mark,'769'))
+        count = count+1;
         l = line([t t], [0  1500]);
         label(l,mark,'location','top');
     end
 end
+% check that count equals 40 (20L/20R)
