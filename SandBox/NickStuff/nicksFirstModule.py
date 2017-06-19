8fa65368025f5fb090483cc2032176c24d1f06a1
George Steelman <gsteelman@olin.edu>
2017-06-19 15:53:58 -0400

    adjusted the main moduel

--- SandBox/NickStuff/nicksFirstModule.py
+++ SandBox/NickStuff/nicksFirstModule.py
@@ -7,13 +7,14 @@ class Main(LatentModule):
 
         # set defaults for some configurable parameters:
         self.trials1 = 5            # number of trials in first part
-        self.trials2 = 20            # number of trials in second part
+        self.trials2 = 300            # number of trials in second part
         self.a_probability = 0.5    # probability that an "A" appears instead of a "U"
 
     def run(self):
         self.rectangle((1,1.5,-.75,-1),duration = 1000,color=(0,0,0,1),block = False)
         self.sleep(45)
-        self.marker(10)  # emit an event marker to indicate the beginning of the experiment
+        self.rectangle((1,1.5,-.75,-1),duration=1,color=(1,1,1,1),block = False)
+        self.marker(700)  # emit an event marker to indicate the beginning of the experiment
 
         self.write('In the second part...I mean first and only part, you will be presented a picture of\na monkey eating a banana. When you see it, close your eyes.\n When you hear the bell, open them',5)
         self.write('The sound of a double bell will indicate the end of the experiment.',2)
@@ -21,20 +22,20 @@ class Main(LatentModule):
         for k in range(self.trials2):
             self.crosshair(1)
             if  self.a_probability > random.random():
-                self.rectangle((1,1.5,-.75,-1),duration=3.5,color=(1,1,1,1),block = False)
+                self.rectangle((1,1.5,-.75,-1),duration=3,color=(1,1,1,1),block = False)
                 self.marker(769)
                 self.picture('monkey.jpg',2,scale=0.3,block = False)
                 self.a_probability = self.a_probability - .1
             else:
-                self.rectangle((1,1.5,-.75,-1),duration=3.5,color=(1,1,1,1),block = False)
+                self.rectangle((1,1.5,-.75,-1),duration=3,color=(1,1,1,1),block = False)
                 self.marker(770)
                 self.picture('tool.jpg',2,scale=0.3,block = False)
                 self.a_probability = self.a_probability + .1
-            self.sleep(2)
+            self.sleep(3)
             self.marker(768)
             self.sound('nice_bell.wav',volume=0.75)
             # wait for a an ISI randomly chosen between 1 and 3 seconds
-            self.sleep(3)
+            self.sleep(5)
 
         self.sound('nice_bell.wav',volume=0.75)
         self.sound('nice_bell.wav',volume=0.75)
