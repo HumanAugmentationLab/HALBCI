8667182d279b4827284ba6731b4a32f6e7c0412e
Sam <samantha.michalka@gmail.com>
2017-07-11 15:28:57 -0400

    Add quick eeglab preprocessing scratchpad updates

--- SandBox/sameeglabepoch.m
+++ SandBox/sameeglabepoch.m
@@ -12,33 +12,41 @@ neaz2 = pop_epoch(feaz2); %events '101' '201' time from 0 to 9 s
 left6hz2 = pop_selectevent(neaz2); % select event type '101', remove others
 right10hz2 = pop_selectevent(neaz2); % select event type '201', remove others
 
+%% Set which data you're lookin at
+left = left6hz;
+right = right15hz;
+
 %% Plot spectr
-figure; pop_spectopo(left6hz2, 1, [0  8998], 'EEG' , 'percent', 100, 'freq', [7 11 15], 'freqrange',[2 25],'electrodes','on');
-figure; pop_spectopo(right10hz2, 1, [0  8998], 'EEG' , 'percent', 100, 'freq', [7 11 15], 'freqrange',[2 25],'electrodes','on');
+figure; pop_spectopo(left, 1, [0  8998], 'EEG' , 'percent', 100, 'freq', [7 11 15], 'freqrange',[2 25],'electrodes','on');
+figure; pop_spectopo(right, 1, [0  8998], 'EEG' , 'percent', 100, 'freq', [7 11 15], 'freqrange',[2 25],'electrodes','on');
 
 %% Plot time freq
-figure; pop_newtimef( left6hz2, 1, 1, [0  8998], [3         0.5] , 'topovec', 1, 'elocs', EEG.chanlocs, 'chaninfo', EEG.chaninfo, 'caption', 'O1', 'baseline',[0], 'freqs', [[2 16]], 'plotphase', 'off', 'padratio', 1);
-figure; pop_newtimef( left6hz2, 1, 1, [0  8998], [3         0.5] , 'topovec', 1, 'elocs', EEG.chanlocs, 'chaninfo', EEG.chaninfo, 'caption', 'O1', 'baseline',[0], 'freqs', [[2 16]], 'plotphase', 'off', 'padratio', 1);
+figure; pop_newtimef( left, 1, 1, [0  8998], [3         0.5] , 'topovec', 1, 'elocs', EEG.chanlocs, 'chaninfo', EEG.chaninfo, 'caption', 'O1', 'baseline',[0], 'freqs', [[2 16]], 'plotphase', 'off', 'padratio', 1);
+figure; pop_newtimef( right, 1, 1, [0  8998], [3         0.5] , 'topovec', 1, 'elocs', EEG.chanlocs, 'chaninfo', EEG.chaninfo, 'caption', 'O1', 'baseline',[0], 'freqs', [[2 16]], 'plotphase', 'off', 'padratio', 1);
 
 
-%
-figure; pop_newtimef( left6hz2, 1, 4, [0  8998], [3         0.5] , 'topovec', 4, 'elocs', EEG.chanlocs, 'chaninfo', EEG.chaninfo, 'caption', 'Pz', 'baseline',[0], 'freqs', [[2 16]], 'plotphase', 'off', 'padratio', 1);
+%figure; pop_newtimef( left6hz2, 1, 4, [0  8998], [3         0.5] , 'topovec', 4, 'elocs', EEG.chanlocs, 'chaninfo', EEG.chaninfo, 'caption', 'Pz', 'baseline',[0], 'freqs', [[2 16]], 'plotphase', 'off', 'padratio', 1);
 
 
 %% Freq analysis for trials
-selchan  = 8;
-figure; [ersp,itc,powbase,times,freqs,erspboot,itcboot tdata] = newtimef(left6hz2.data(selchan,:,:), 4500,[0  8998],500, [3  0.5] , 'freqs', [[2 16]],'nfreqs',8,'ntimesout', 9);
+selchan  = 4;
+figure; [ersp,itc,powbase,times,freqs,erspboot,itcboot tdata] = newtimef(left.data(selchan,:,:), 4500,[0  8998],500, [3  0.5] , 'freqs', [[2 16]],'nfreqs',8,'ntimesout', 9);
 % tdata is the freq for each trial
 tdatapower = abs(tdata).^2;
 
-figure; [ersp,itc,powbase,times,freqs,erspboot,itcboot righttdata] = newtimef(right10hz2.data(selchan,:,:), 4500,[0  8998],500, [3  0.5] , 'freqs', [[2 16]],'nfreqs',8,'ntimesout', 9);
+figure; [ersp,itc,powbase,times,freqs,erspboot,itcboot righttdata] = newtimef(right.data(selchan,:,:), 4500,[0  8998],500, [3  0.5] , 'freqs', [[2 16]],'nfreqs',8,'ntimesout', 9);
 righttdatapower = abs(righttdata).^2;
 
 % Histogram of freq power
-selfreq = 7; %index in freq variable
+selfreq = 4; %index in freq variable
 figure
 tempdata = [reshape(tdatapower(selfreq,:,:),size(tdatapower,2)*size(tdatapower,3),1)...
     reshape(righttdatapower(selfreq,:,:),size(tdatapower,2)*size(tdatapower,3),1)];
-hist(tempdata,50)
+hist(tempdata,10)
 xlim([0 5e5])
+title(num2str(freqs(selfreq)))
+
+%% Compare
+figure; [ersp,itc,powbase,times,freqs,erspboot,itcboot tdata] = newtimef({left.data(selchan,:,:) right.data(selchan,:,:)}, 4500,[0  8998],500, [3  0.5] , 'freqs', [[4 18]],'nfreqs',2,'ntimesout', 4);
+
 
