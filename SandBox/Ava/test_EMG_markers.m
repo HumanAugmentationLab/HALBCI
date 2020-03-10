257b10bfab356e9dd762ab0a6195d68e19a50bb0
alakmazaheri <avalakmazaheri@gmail.com>
2020-03-10 09:34:09 -0400

    Marker stream for OpenBCI demo

--- SandBox/Ava/test_EMG_markers.m
+++ SandBox/Ava/test_EMG_markers.m
@@ -1,3 +1,7 @@
+
+addpath('C:\Users\alakmazaheri\Documents\BCI\labstreaminglayer\Apps\MATLAB Viewer')
+
+
 %% Write to LSL
 addpath(genpath('/home/hal/Research/Matlab/BCILAB/dependencies/liblsl-Matlab'));
 
@@ -5,21 +9,32 @@ disp('Loading library...');
 lib = lsl_loadlib();
 
 disp('Creating new marker stream info')
-info = lsl_streaminfo(lib, 'PsychMarkers', 'Markers', 1, 0, 'cf_int32', 'mysourceid');
+info = lsl_streaminfo(lib, 'EMGMarkers', 'Markers', 1, 0, 'cf_int32', 'mysourceid');
 
 disp('Opening an outlet...')
 outlet = lsl_outlet(info);
 
-for gestureMarker = 1:10
+pause(5);
+disp('Starting')
+pause(1);
+
+a = 2; b = 3;
+n = 25;
+% markers = [zeros(n, 1); ones(n, 1)];
+markers = repmat([0 1], 1, n);
+% markers_shuffle = markers(randperm(length(markers)));
+
+for i = 1:n*2
+    gestureMarker = markers(i);
     outlet.push_sample(gestureMarker);
     disp(gestureMarker);
-    pause(2);
+    pause_dur = (b-a).*rand(1,1) + a;
+    pause(pause_dur);
 end
 
-
+disp('Done')
 %% Read from LSL
 
-% streams = load_xdf('C:/Users/alakmazaheri/Desktop/dummy.xdf')
 
 % instantiate the library
 disp('Loading the library...');
@@ -29,7 +44,7 @@ lib = lsl_loadlib();
 disp('Resolving a Markers stream...');
 result = {};
 while isempty(result)
-    result = lsl_resolve_byprop(lib,'type','Markers'); end
+    result = lsl_resolve_byprop(lib,'type','EEG'); end
 
 % create a new inlet
 disp('Opening an inlet...');
@@ -40,5 +55,16 @@ while true
     % get data from the inlet
     [mrks,ts] = inlet.pull_sample();
     % and display it
-    fprintf('got %s at time %.5f\n',mrks{1},ts);
+    fprintf('got %s at time %.5f\n',mrks,ts);
 end
+
+%% Read from XDF
+streams = load_xdf('C:/Users/alakmazaheri/Desktop/test.xdf')
+streams = exp_eval(io_loadset('C:/Users/alakmazaheri/Desktop/test.xdf'))
+
+n = 2;
+plot(streams{1,n}.time_stamps, streams{1,n}.time_series');
+hold on
+% for i = 1:length(streams{1,1}.time_stamps)
+%    vline(streams{1,1}.time_stamps(i))
+% end
